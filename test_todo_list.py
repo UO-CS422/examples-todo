dce8d43b371047f10a11c06021dcd774f57e9aac
Alea M <158105943+Akminar@users.noreply.github.com>
2025-02-24 15:44:17 -0800

    Todo unit tests

---
+++ test_todo_list.py
@@ -0,0 +1,21 @@
+from todo.todo_list import ToDoList
+
+class TestToDoList:
+    """Unit test for the ToDoList class."""
+    def __init__(self) -> None:
+        self.test_results = []
+
+    def test_init(self) -> bool:
+        """Test the __init__ function"""
+
+    def test_add_task(self, task: str) -> bool:
+        """Test the add_task function"""
+
+    def test_get_task(self, index: int) -> bool:
+        """Test the get_task function"""
+
+    def test_remove_task(self, index: int) -> bool:
+        """Test the remove_task function"""
+
+    def test_all(self) -> str:
+        """Test all of the functions in ToDoList"""

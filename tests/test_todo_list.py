fa70f087b2641a99cc3a9ced28b8de0c8e2515ac
unknown <charlieohudson@gmail.com>
2025-02-24 15:18:46 -0800

    first test cases

--- tests/test_todo_list.py
+++ tests/test_todo_list.py
@@ -1,18 +1,6 @@
 import pytest
 from todo.todo_list import ToDoList
 
-def test_add_task():
-    todo = ToDoList()
-    task = "Write unit tests"
-    index = todo.add_task(task)
-    assert index == 0
-    assert todo.tasks == [task]
-
-def test_add_task_invalid():
-    todo = ToDoList()
-    with pytest.raises(ValueError, match="Task must be a non-empty string."):
-        todo.add_task("")  # Testing with an empty string
-
 def test_get_task():
     todo = ToDoList()
     task = "Write unit tests"
@@ -24,15 +12,39 @@ def test_get_task_invalid_index():
     with pytest.raises(IndexError, match="Task index out of range."):
         todo.get_task(1)  # No task at index 1
 
-def test_remove_task():
-    todo = ToDoList()
-    task = "Write documentation"
-    todo.add_task(task)
-    removed_task = todo.remove_task(0)
-    assert removed_task == task
-    assert len(todo.tasks) == 0
 
 def test_remove_task_invalid_index():
     todo = ToDoList()
     with pytest.raises(IndexError, match="Task index out of range."):
         todo.remove_task(0)  # No task to remove
+
+def test_todo_list_add_task():
+    """
+    GIVEN a todo list instance
+    WHEN I call the add task method.
+    THEN the task should be added to the todo list.
+    """
+    test_list = ToDoList()
+    test_list.add_task("Write test cases")
+    assert test_list.tasks[0] == "Write test cases"
+
+def test_todo_list_remove_task():
+    """
+    GIVEN a todo list
+    WHEN I call the remove task method
+    THEN a task will be removed.
+    """
+    test_list = ToDoList()
+    test_list.tasks = ["foo", "bar"]
+    test_list.remove_task(1)
+    assert test_list.tasks == ["foo"]
+
+def test_todo_list_raise_value_error():
+    """
+    GIVEN I add a task to a todo list
+    WHEN the task is non a valid string
+    THEN a value error is raised.
+    """
+    test_list = ToDoList()
+    with pytest.raises(ValueError):
+        test_list.add_task(0)

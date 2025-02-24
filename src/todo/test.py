146f1f6e9e9c3ef9f8d5fc68471c7d5d95a4f5a0
digital verdigris <montgomery.austin00@gmail.com>
2025-02-24 15:09:23 -0800

    add task test

--- src/todo/test.py
+++ src/todo/test.py
@@ -5,6 +5,12 @@ class test_todo_list(unittest.TestCase):
     def test_init(self):
         test_list = ToDoList()
         self.assertEqual(test_list.tasks, [])
+    
+    def test_add_task(self):
+        test_list = ToDoList()
+        test_list.add_task('ayo')
+        self.assertEqual(test_list.tasks, ['ayo'])
+
 
 if __name__ == '__main__':
     unittest.main()

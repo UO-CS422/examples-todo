cda75c7293a94a6678ab6a3b9277b8100f1ebb67
cadenweigel <157074200+cadenweigel@users.noreply.github.com>
2025-02-26 14:58:49 -0800

    test remove

--- src/todo/test_todo.py
+++ src/todo/test_todo.py
@@ -12,3 +12,9 @@ def test_todo_list_add_task():
     test_list = ToDoList()
     test_list.add_task("task 1")
     assert test_list.tasks[0] == "task 1"
+
+def test_todo_list_remove_task():
+    test_list = ToDoList()
+    test_list.tasks = ["task 1", "task 2"]
+    test_list.remove_task(1)
+    assert test_list.tasks == ["task 1"]

4b5b1353cd6996f12d4c04af692f6e09f1cc9dfa
cadenweigel <157074200+cadenweigel@users.noreply.github.com>
2025-02-26 14:58:09 -0800

    test add

--- src/todo/test_todo.py
+++ src/todo/test_todo.py
@@ -7,3 +7,8 @@ def todo():
 def test_initialize(todo):
     assert isinstance(todo, ToDoList)
     assert todo.tasks.empty()
+
+def test_todo_list_add_task():
+    test_list = ToDoList()
+    test_list.add_task("task 1")
+    assert test_list.tasks[0] == "task 1"

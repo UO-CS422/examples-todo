d8143d09e940da0717e1982bab42b67247b6d521
Boyana Norris <brnorris03@gmail.com>
2025-03-03 14:22:02 -0800

    move test to tests folder

--- tests/test_todo_list.py
+++ tests/test_todo_list.py
@@ -36,3 +36,24 @@ def test_remove_task_invalid_index():
     todo = ToDoList()
     with pytest.raises(IndexError, match="Task index out of range."):
         todo.remove_task(0)  # No task to remove
+from todo.todo_list import ToDoList
+
+def test_constructor():
+    list = ToDoList()
+    assert list.tasks == []
+
+def test_add():
+    list = ToDoList()
+    list.add_task("test")
+    assert list.tasks == ["test"]
+
+def test_get():
+    list = ToDoList()
+    list.add_task("test")
+    assert list.get_task(0) == "test"
+
+def test_remove():
+    list = ToDoList()
+    list.add_task("test")
+    list.remove_task(0)
+    assert list.tasks == []

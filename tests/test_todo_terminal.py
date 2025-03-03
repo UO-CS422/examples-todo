87d89581d44cc50f68a08fa2942e00209cb10b9f
ameliabates <bates.amelia@gmail.com>
2025-03-03 14:17:11 -0800

    test

---
+++ tests/test_todo_terminal.py
@@ -0,0 +1,10 @@
+import unittest
+from src.todo.todo_terminal import TodoTerminal
+
+class MyTestCase(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(TodoTerminal, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
